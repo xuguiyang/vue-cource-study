04a0e4593c9ecee70bcc78f9707b9fde1ca75d4c
xuguiyang <315028457@qq.com>
2019-05-15 09:42:41 +0800

    1

---
+++ final.cmd
@@ -0,0 +1,2 @@
+props用法
+https://www.jb51.net/article/143049.htm
--- vue-cource/src/views/Home.vue
+++ vue-cource/src/views/Home.vue
@@ -28,7 +28,7 @@ export default {
   props: {
     food: {
       type: String,
-      default: "apple"
+      default: "apple,banana"
     }
   },
   //渲染该组件对应路由被确认前调用
