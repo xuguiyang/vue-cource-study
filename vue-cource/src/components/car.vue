c77c6157f330bb8839ad75960cf1fefe5f175092
xuguiyang <315028457@qq.com>
2019-08-14 09:30:43 +0800

    使用原生事件的购物车悬浮

--- vue-cource/src/components/car.vue
+++ vue-cource/src/components/car.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="ys-float-btn" :style="{'width':itemWidth+'px','height':itemHeight+'px','left':left+'px','top':top+'px'}"
-       ref="div"
-
-       >
-    <p>{{text}}</p>
+  <div class="floatcar"
+    @touchstart="handleTouchStart"
+    @touchmove.prevent="handleTouchMove"
+    @touchend="handleTouchEnd"
+    :style="{'width':itemWidth+'px','height':itemHeight+'px','left':left+'px','top':top+'px'}"
+    ref="div">
+      <div class="iconfont icon-gouwuchekong shopcar" @click="gocar"></div>
   </div>
 </template>
 
@@ -11,10 +13,6 @@
   export default {
     name: "car",
     props:{
-      text:{
-        type:String,
-        default:"购物车"
-      },
       itemWidth:{
         type:Number,
         default:60
@@ -23,133 +21,95 @@
         type:Number,
         default:60
       },
-      gapWidth:{
+      gapWidth:{ // car距离边界宽度
         type:Number,
-        default:10
+        default:0
       },
       coefficientHeight:{
         type:Number,
-        default:0.8
+        default:0.6
+      }
+    },
+     data(){
+      return{
+        left:0,
+        top:0,
+        startToMove: false,
+        clientW: document.documentElement.clientWidth,
+        clientH: document.documentElement.clientHeight
       }
     },
     created(){
-      this.clientWidth = document.documentElement.clientWidth;
-      this.clientHeight = document.documentElement.clientHeight;
-      this.left = this.clientWidth - this.itemWidth - this.gapWidth;
-      this.top = this.clientHeight*this.coefficientHeight;
-      //  console.log('clientWidth',this.clientWidth)
-      // console.log('clientHeight',this.clientHeight)
-      // console.log('left',this.left)
-      // console.log('top',this.top)
-    },
-    mounted(){
-      window.addEventListener('scroll', this.handleScrollStart);
-      this.$nextTick(()=>{
-        const div = this.$refs.div;
-        div.addEventListener("touchstart",()=>{
-          div.style.transition = 'none';
-        });
-        div.addEventListener("touchmove",(e)=>{
-          if (e.targetTouches.length === 1) {
-            let touch = event.targetTouches[0];
-            this.left = touch.clientX - this.itemWidth/2;
-            this.top = touch.clientY - this.itemHeight/2;
-          }
-        });
-        div.addEventListener("touchend",()=>{
-          div.style.transition = 'all 0.3s';
-           if(this.left>this.clientWidth/2){
-             this.left = this.clientWidth - this.itemWidth - this.gapWidth;
-
-             if(this.top <0) {
-               this.top = 0
-             }
-             if(this.top > this.clientHeight -this.itemHeight){this.top = this.clientHeight-this.itemHeight;}
-            //  if(this.top > this.clientHeight -this.itemHeight){this.top = this.clientHeight*this.coefficientHeight;}
-            //  console.log('this.left',this.left)
-
-            //  console.log('this.clientWidth',this.clientWidth)
-             console.log('this.clientHeight',this.clientHeight)
-             console.log('this.top',this.top)
-             console.log('this.gapWidth',this.gapWidth)
-           }else{
-             this.left = this.gapWidth;
-           }
-        });
-
-      });
-    },
-    beforeDestroy(){
-      window.removeEventListener('scroll', this.handleScrollStart);
+      this.clientX = document.documentElement.clientWidth;
+      this.clientY = document.documentElement.clientHeight;
+      this.left = this.clientX - this.itemWidth - this.gapWidth;
+      this.top = (this.clientH/2 - this.itemHeight/2)
     },
     methods:{
-      // onBtnClicked(){
-      //   this.$emit("onFloatBtnClicked");
-      // },
-      // handleScrollStart(){
-      //   this.timer&&clearTimeout(this.timer);
-      //   this.timer = setTimeout(()=>{
-      //     this.handleScrollEnd();
-      //   },300);
-      //   this.currentTop = document.documentElement.scrollTop || document.body.scrollTop;
-      //   if(this.left>this.clientWidth/2){
-      //     this.left = this.clientWidth - this.itemWidth/2;
-      //   }else{
-      //     this.left = -this.itemWidth/2;
-      //   }
-      // },
-      // handleScrollEnd(){
-      //   let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-      //   if(scrollTop === this.currentTop){
-      //     if(this.left>this.clientWidth/2){
-      //       this.left = this.clientWidth - this.itemWidth - this.gapWidth;
-      //     }else{
-      //       this.left = this.gapWidth;
-      //     }
-      //     clearTimeout(this.timer);
-      //   }
-      // }
+      gocar() {
+        this.$router.push({
+          path: "car"
+        })
+      },
+      //开始
+      handleTouchStart() {
+        this.startToMove = true
+        this.$refs.div.style.transition = "none"
+      },
+      //拖动
+      handleTouchMove(e) {
+        const clientX = e.targetTouches[0].clientX
+        const clientY = e.targetTouches[0].clientY
+        const isInScreen = clientX <= this.clientW && clientX >= 0 && clientY <= this.clientH && clientY >=0
+        if(this.startToMove && e.targetTouches.length === 1) {
+            if(isInScreen) {
+                this.left = clientX - this.itemWidth/2
+                this.top = clientY - this.itemHeight/2
+            }
+        }
+      },
+      //结束
+      handleTouchEnd() {
+        if(this.left < (this.clientW / 2)) {
+            this.left = 0
+            this.handleIconY()
+        } else {
+            this.left = this.clientW - this.itemWidth
+            this.handleIconY()
+        }
+        this.$refs.div.style.transition = "all .3s"
+      },
+       //上下边界点判断
+      handleIconY() {
+        if (this.top < 0) {
+            this.top = this.itemHeight
+        } else if(this.top + this.itemHeight > this.clientH) {
+            this.top = this.clientH - this.itemHeight*2
+        }
+      },
     },
-    data(){
-      return{
-        timer:null,
-        currentTop:0,
-        clientWidth:0,
-        clientHeight:0,
-        left:0,
-        top:0,
-      }
-    }
   }
 </script>
 
 <style lang="less" scoped>
-  .ys-float-btn{
-    // background:rgb(255,255,255);
-    background-color: #ccc;
+  .floatcar{
+    background:rgb(255,255,255);
     box-shadow:0 2px 10px 0 rgba(0,0,0,0.1);
     border-radius:50%;
     color: #666666;
-    z-index: 20;
-    transition: all 0.3s;
-
+    z-index: 999;
+    transition: all 0.2s;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
     position: fixed;
-    bottom: 20vw;
-
-    img{
-      width: 50%;
-      height: 50%;
-      object-fit: contain;
-      margin-bottom: 3px;
-    }
-
-    p{
-      font-size:7px;
+    bottom: 2rem;
+    .shopcar{
+      width: 100%;
+      text-align: center;
+      font-size:35px;
+      color: red;
     }
   }
 </style>
